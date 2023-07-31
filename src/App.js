81ac9286fd05421c5aa73255761360fc26fe0ed2
Akshaybaisware <akshaybaisware1998@gmail.com>
2023-07-31 10:52:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () =>  {
+ const [latitude , setLatitude]= useState("");
+ const [longitude , setLongitude] =useState("");
+ const [hemisphere , setHemisphere] =useState("");
+ const [month , setMonth]= useState(new Date().getMonth());
+
+
+
+function getgeolocation()
+{
+    // if (navigator.geolocation) // ye inbuild method from javascript
+    //{
+         navigator.geolocation.getCurrentPosition( // ye teeno inbuild method from javascriot
+            (location) =>{
+                //latitude and longitude ki value store krne ke liye use state use hui hai
+                        setLatitude(location.coords.latitude); // lat
+                        setLongitude(location.coords.longitude);
+          
+                        if(latitude>0)
+                        {
+                        setHemisphere("Northern Hemisphere")
+                        }
+               
+                        else if(latitude<0)
+                        {
+                           setHemisphere("Southern Hemisphere")
+                        }
+                        else {
+                           setHemisphere("Equator")
+                        }
+                    }
+         )
+
+
+         
+    //}
 }
 
+    return    (
+        <div>
+               <h1>Latitude:{latitude}</h1>
+               <h1>Longitude:{longitude}</h1>
+               <h1>Hemisphere:{hemisphere}</h1>
+               <h1>Month:{month}</h1>
+           <button onClick={getgeolocation}>Get locationn</button>
+        </div>
+    )
+}
 export default App;
